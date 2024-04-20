06142e4df0507e868e5dbceae0dfc3fe6694db8e
dathphan <167285212+dathphan@users.noreply.github.com>
2024-04-19 20:18:10 -0700

    Update Monster.js

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -7,6 +7,32 @@ class Monster extends Phaser.Scene {
         this.bodyX = 300;
         this.bodyY = 350;
         
+        this.armLeftX = 250;
+        this.armLeftY = 410;
+        
+        this.armRightX = 350;
+        this.armRightY = 410;
+        
+        this.legLeftX = 260;
+        this.legLeftY = 480;
+        
+        this.legRightX = 310;
+        this.legRightY = 495;
+        
+        this.earLeftX = 250;
+        this.earLeftY = 260;
+        
+        this.earRightX = 340;
+        this.earRightY = 250;
+        
+        this.eyeX = 250;
+        this.eyeY = 320;
+
+        this.mouthX = 250;
+        this.mouthY = 370;
+
+        this.offsetX = 0;
+        this.offsetY = 0;
     }
 
     // Use preload to load art and sound assets before the scene starts running.
@@ -31,15 +57,54 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
 
-        
+        // Back Side
+        my.sprite.leftArm = this.add.sprite(this.armLeftX + this.offsetX,this.armLeftY + this.offsetY, "monsterParts", "arm_whiteE.png");
+        my.sprite.legLeft = this.add.sprite(this.legLeftX + this.offsetX,this.legLeftY + this.offsetY, "monsterParts", "leg_darkC.png");
+        my.sprite.legLeft.flipX = true;
+        my.sprite.leftEar = this.add.sprite(this.earLeftX + this.offsetX,this.earLeftY + this.offsetY, "monsterParts", "detail_white_ear.png");
+        my.sprite.leftEar.flipX = true;
+        my.sprite.leftEar.scale = 1.5;
+
+        // Body
+        my.sprite.body = this.add.sprite(this.bodyX + this.offsetX, this.bodyY + this.offsetY, "monsterParts", "body_blueF.png");
+
+        // Front
+        my.sprite.legRight = this.add.sprite(this.legRightX + this.offsetX,this.legRightY + this.offsetY, "monsterParts", "leg_darkC.png");
+        my.sprite.legRight.flipX = true;
+        my.sprite.rightArm = this.add.sprite(this.armRightX + this.offsetX,this.armRightY + this.offsetY, "monsterParts", "arm_whiteE.png");
+        my.sprite.rightEar = this.add.sprite(this.earRightX + this.offsetX,this.earRightY + this.offsetY, "monsterParts", "detail_white_ear.png");
+        my.sprite.rightEar.scale = 1.6
+
+        // Face
+        my.sprite.eye = this.add.sprite(this.eyeX + this.offsetX, this.eyeY + this.offsetY, "monsterParts", "eye_cute_light.png");
+        my.sprite.mouth = this.add.sprite(this.mouthX + this.offsetX, this.mouthY + this.offsetY, "monsterParts", "mouthH.png");
+        my.sprite.mouth.scale = 0.6;
+
+        this.input.keyboard.on("keydown", function (event) {
+            if (event.code == "KeyD") {
+                for (const part in my.sprite){
+                    my.sprite[part].x += 25
+                }
+            }
+            else if (event.code == "KeyA") {
+                for (const part in my.sprite){
+                    my.sprite[part].x -= 25
+                }
+            }
+            else if (event.code == "KeyS"){
+                my.sprite.mouth.setFrame("mouthH.png");
+            }
+            else if (event.code == "KeyF"){
+                my.sprite.mouth.setFrame("mouth_closed_fangs.png");
+            }
+        });
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
-
-       
+        
+        
     }
 
 }
